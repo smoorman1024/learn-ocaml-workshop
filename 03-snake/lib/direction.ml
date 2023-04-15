de4a3facfd645552496f143b4d0f2ed1ce1f747b
Sean Moorman <smoorman1024@github.com>
2023-04-15 15:16:54 -0400

    implement and test direction

--- 03-snake/lib/direction.ml
+++ 03-snake/lib/direction.ml
@@ -7,8 +7,29 @@ type t =
   | Down
 [@@deriving sexp_of]
 
-(* TODO: Implement [next_position].
+let next_position t (p : Position.t) =
+  match t with
+  | Left  -> { p with col = p.col - 1 }
+  | Right -> { p with col = p.col + 1 }
+  | Up    -> { p with row = p.row + 1 }
+  | Down  -> { p with row = p.row - 1 }
 
-   Make sure to take a look at the signature of this function to understand what it does.
-   Recall that the origin of the board is in the lower left hand corner. *)
-let next_position t position = position
+let%test "Testing Left..." =
+  let p0 : Position.t = {row=5; col=5} in
+  let np0 : Position.t = next_position Left p0 in
+  Int.equal np0.row 5 && Int.equal np0.col 4
+
+let%test "Testing Right..." =
+  let p0 : Position.t = {row=5; col=5} in
+  let np0 : Position.t = next_position Right p0 in
+  Int.equal np0.row 5 && Int.equal np0.col 6
+
+let%test "Testing Up..." =
+  let p0 : Position.t = {row=5; col=5} in
+  let np0 : Position.t = next_position Up p0 in
+  Int.equal np0.row 6 && Int.equal np0.col 5
+
+let%test "Testing Down..." =
+  let p0 : Position.t = {row=5; col=5} in
+  let np0 : Position.t = next_position Down p0 in
+  Int.equal np0.row 4 && Int.equal np0.col 5
